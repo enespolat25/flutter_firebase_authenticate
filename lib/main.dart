c74e59b998bfce1934e30e3b82089ba98f3f4572
enes polat <enespolat@macbook-pro.zte.com.cn>
2020-06-30 08:58:59 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'root_page.dart';
+import 'LoginSignupPage.dart';
+import 'authentication.dart';
+void main() => runApp(MyApp());
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Authentication AndroidVille',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: RootPage(
+        auth: new Auth(),
+      ),
+    );
+  }
+}
